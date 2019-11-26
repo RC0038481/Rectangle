5a5054c59c52027742e0aef9ff10896e32752104
George Mannion <rc0038481@student.riversidecollege.ac.uk>
2019-11-26 12:44:36 +0000

    Add project files.

---
+++ Rectangle/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rectangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = (Convert.ToInt32(textBox3.Text) * Convert.ToInt32(textBox2.Text)).ToString();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            double width = double.Parse(textBox2.Text);
+            double height = double.Parse(textBox3.Text);
+            textBox1.Text = String.Empty;
+            textBox1.Text = width * height;
+        }
+    }
+}
